57734cbe188c88dc0b00bf1e1a474d579df4668a
Kieran <Kieran@Kieran-HP.home>
2018-03-27 12:21:38 +0100

    Linked Node addition

--- src/queuemanager/PriorityItem.java
+++ src/queuemanager/PriorityItem.java
@@ -9,6 +9,7 @@ public class PriorityItem<T> {
 
     private final T item;
     private final int priority;
+    private PriorityItem<T> next;
 
     public PriorityItem(T item, int priority) {
         this.item = item;
@@ -22,9 +23,20 @@ public class PriorityItem<T> {
     public int getPriority() {
         return priority;
     }
-
+    
     @Override
     public String toString() {
         return "(" + getItem() + ", " + getPriority() + ")";
+    }
+    
+    public PriorityItem<T> getNext() {
+        return next;
+    }
+    
+  
+    public PriorityItem(T item, int priority, PriorityItem<T> next) {
+        this.item = item;
+        this.next = next;
+        this.priority = priority;
     }
 }
